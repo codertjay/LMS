0151ebc9c96167fa374cb15524b79c7ebb768a4e
Afenikhena Favour <codertjay@gmail.com>
2021-02-04 21:16:52 +0100

    ii

--- Learning_platform/settings.py
+++ Learning_platform/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 if DEBUG:
-    ALLOWED_HOSTS = ['127.0.0.1', '.localhost', 'localhost']
+    ALLOWED_HOSTS = ['127.0.0.1', '.localhost', 'localhost','142.93.49.230']
 else:
     ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # Application definition
