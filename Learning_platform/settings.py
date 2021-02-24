ec638651252df7b50bfcb17c7842ea2fd9d7fe95
Afenikhena Favour <63492224+codertjay@users.noreply.github.com>
2021-02-24 14:18:56 +0100

    Update settings.py
    Ss

--- Learning_platform/settings.py
+++ Learning_platform/settings.py
@@ -186,7 +186,7 @@ SITE_ID = 1
 
 # for sending email
 
-EMAIL_BACKEND ='EMAIL_BACKEND ="sendgrid_backend.SendgridBackend"
+EMAIL_BACKEND ="sendgrid_backend.SendgridBackend"
 
 SENDGRID_API_KEY = config('SENDGRID_API_KEY')
 
