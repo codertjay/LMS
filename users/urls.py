f15e270c9254af1c9ef8276b9b5ff8228db3b775
Afenikhena Favour <codertjay@gmail.com>
2021-04-26 11:51:08 +0100

    cc

--- users/urls.py
+++ users/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('contact/', contactAdminView, name='contact'),
 
     # academy profile
-    path('profile/<str:username>/', test_p, name='profile'),
-    # path('profile/<str:username>/', public_profile_view, name='profile'),
+    # path('profile/<str:username>/', test_p, name='profile'),
+    path('profile/<str:username>/', public_profile_view, name='profile'),
     path('profile_update/', UserProfileUpdate.as_view(), name='profile_edit'),
     path('profile_update/', UserProfileUpdate.as_view(), name='profile_edit'),
 
