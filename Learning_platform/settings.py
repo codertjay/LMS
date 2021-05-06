22f28c844b3a51ccc1c2b5aa442667864884a014
Afenikhena Favour <codertjay@gmail.com>
2021-05-06 10:08:13 +0100

    dd

--- Learning_platform/settings.py
+++ Learning_platform/settings.py
@@ -265,7 +265,7 @@ VIMEO_AUTHENTICATE = vimeo.VimeoClient(
     secret="brfCb290lRxOGM0fFAHu1NATjxiDsClBHEVd6Sa1l8Sa1Jpm4PEDmCkC+51T5dRkJtXVT6efhDVp894ZMTUqcgQ5XBYtLxHZHPzNl4+LmFuYFkmk8ThPXzkOucAqXlFN"
 )
 newsapi = NewsApiClient(api_key= config('NewApiKey', default=''))
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     'http://www.assassinfx.com',
